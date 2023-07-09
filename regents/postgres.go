1a2613007381e07b56b80855ab500b6e5a2bd418
Nick James <nickjames8806@gmail.com>
2023-07-09 13:30:01 -0700

    6 - Create beginnings of a Regent for Postgres.

---
+++ regents/postgres.go
@@ -0,0 +1,44 @@
+package regents
+
+import (
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/putnam120/postgRegent/database"
+	"github.com/putnam120/postgRegent/database/rbac"
+)
+
+type PostgresRegent struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Dbname   string
+	connPool *pgxpool.Pool
+}
+
+func (r *PostgresRegent) Init() error {
+	if err := r.connect(); err != nil {
+		return err
+	}
+	if err := rbac.DisablePublic(r.connPool, r.Dbname); err != nil {
+		return err
+	}
+
+	fmt.Println("Successfully created regent for host: ", r.Host)
+	return nil
+}
+
+func (r *PostgresRegent) connect() error {
+	if len(r.Dbname) == 0 {
+		r.Dbname = r.User
+	}
+	conn, err := database.Connect(r.Host, r.Port, r.User, r.Password, r.Dbname)
+
+	if err != nil {
+		return err
+	}
+
+	r.connPool = conn
+	return nil
+}
