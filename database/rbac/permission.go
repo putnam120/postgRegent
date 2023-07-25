b73b4b4da8bcbf1efbd9ae6dd970d2097b458cf6
Nick James <nickjames8806@gmail.com>
2023-07-24 21:07:23 -0700

    Add ability to create roles with EDIT and ADMIN permissions.

--- database/rbac/permission.go
+++ database/rbac/permission.go
@@ -14,6 +14,8 @@ type Action string
 type actions struct {
 	CONNECT Action
 	READ    Action
+	EDIT    Action
+	ADMIN   Action
 }
 
 type Permission struct {
@@ -27,6 +29,8 @@ type Permission struct {
 var Actions = actions{
 	CONNECT: "CONNECT",
 	READ:    "READ",
+	EDIT:    "EDIT",
+	ADMIN:   "ADMIN",
 }
 
 func CreatePermission(pool *pgxpool.Pool, permission *Permission) error {
@@ -46,8 +50,10 @@ func CreatePermission(pool *pgxpool.Pool, permission *Permission) error {
 	}
 
 	// Add ability to read data in the given (database, schema, table) tuples.
-	if actionsSet.Contains(Actions.READ) {
-		fmt.Println("Aciton set contains READ")
+	if actionsSet.Contains(Actions.READ) &&
+		!(actionsSet.Contains(Actions.EDIT) || actionsSet.Contains(Actions.ADMIN)) {
+
+		fmt.Println("Aciton set for a READER")
 		queries = append(queries,
 			fmt.Sprintf("GRANT SELECT ON %s IN SCHEMA %s TO %s;",
 				strings.Join(permission.Tables[:], ","),
@@ -59,6 +65,38 @@ func CreatePermission(pool *pgxpool.Pool, permission *Permission) error {
 				permission.Name,
 			),
 		)
+	}
+
+	// Add ability to edit (but not delete) data in the given (database, schema, table) tubles.
+	if actionsSet.Contains(Actions.EDIT) && !actionsSet.Contains(Actions.ADMIN) {
+		fmt.Println("Action set for an EDITOR")
+		queries = append(queries,
+			fmt.Sprintf("GRANT SELECT,INSERT,UPDATE ON %s IN SCHEMA %s TO %s;",
+				strings.Join(permission.Tables[:], ","),
+				strings.Join(permission.Schemas[:], ","),
+				permission.Name,
+			),
+			fmt.Sprintf("GRANT USAGE ON SCHEMA %s TO %s;",
+				strings.Join(permission.Schemas[:], ","),
+				permission.Name,
+			),
+		)
+	}
+
+	if actionsSet.Contains(Actions.ADMIN) {
+		fmt.Println("Action set for an ADMIN")
+		queries = append(queries,
+			fmt.Sprintf("GRANT ALL ON %s IN SCHEMA %s TO %s;",
+				strings.Join(permission.Tables[:], ","),
+				strings.Join(permission.Schemas[:], ","),
+				permission.Name,
+			),
+			fmt.Sprintf("GRANT ALL ON SCHEMA %s TO %s;",
+				strings.Join(permission.Schemas[:], ","),
+				permission.Name,
+			),
+		)
+
 	}
 
 	createQuery := strings.Join(queries[:], "")
